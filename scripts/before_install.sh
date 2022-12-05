a90068c631d481e3f43279524710e1a2b1915275
Govindaraj-B <govi1012000@gmail.com>
2022-12-05 16:34:16 +0530

    strapi try

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-dir=/home/ubuntu/app
-if [ ! -d $dir ]
-then
-     mkdir $dir
-else
-     echo "Directory exists"
-fi
+mkdir -p /home/ubuntu/app
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
