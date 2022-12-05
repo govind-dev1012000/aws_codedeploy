b5e0801eee0945e2f8cbb6c59cba4fd791a8fafc
Govindaraj-B <govi1012000@gmail.com>
2022-12-05 16:26:22 +0530

    init try

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd /home/ubuntu/server
+dir=/home/ubuntu/app
+if [ ! -d $dir ]
+then
+     mkdir $dir
+else
+     echo "Directory exists"
+fi
 curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
