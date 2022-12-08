172622ffe2c922aff88cdb05b06a99dd8de00ff7
Govindaraj-B <govi1012000@gmail.com>
2022-12-08 07:49:38 +0530

    all

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd /home/ubuntu/app
-sudo systemctl stop myApp
+IS_ACTIVE=$(sudo systemctl is-active myApp)
+if [ "$IS_ACTIVE" == "active" ]; then
+    # restart the service
+    echo "Service is running"
+    echo "Stop the service"
+    sudo systemctl stop myApp
+else
+    # create service file
+    echo "Service file already exist"
+fi
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,39 @@
 sudo mkdir -p /home/ubuntu/app
 sudo curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+IS_ACTIVE=$(sudo systemctl is-active myApp)
+if [ "$IS_ACTIVE" == "active" ]; then
+    # restart the service
+    echo "Service is running"
+    echo "Restarting service"
+    sudo systemctl restart myApp
+    echo "Service restarted"
+else
+    # create service file
+    echo "Creating service file"
+    sudo cat > /etc/systemd/system/myApp.service << EOF
+[Unit]
+Description=This service will run npm start
+After=network.target
+
+
+
+[Service]
+User=ubuntu
+Group=www-data
+
+
+
+WorkingDirectory=/home/ubuntu/app
+ExecStart=npm start
+[Install]
+WantedBy=multi-user.target
+EOF
+    # restart daemon, enable and start service
+    echo "Reloading daemon and enabling service"
+    sudo systemctl daemon-reload 
+    sudo systemctl enable myApp # remove the extension
+    sudo systemctl start myApp
+    echo "Service Started"
+fi
